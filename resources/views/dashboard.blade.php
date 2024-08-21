1620eaed696396bfe722139b9d3d28009f5565a0
sunuraihan <sunuraihan29@gmail.com>
2024-08-21 17:46:31 +0700

    chore: Update buttons and sidebar icons for Desain section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,22 +20,35 @@
         <div class="card">
             <div class="card-body">
                 <div class="row d-flex justify-content-center align-items-center">
-                    <div class="col-md-5 col-12 d-flex justify-content-center">
-                        <button class="btn btn-primary col-md-5">
-                            <a href="#" class="text-white" style="font-size: 20px">
+                    <!-- Button 1 -->
+                    <div class="col-md-4 col-12 d-flex justify-content-center">
+                        <button class="btn btn-primary" style="width: 100%;">
+                            <a href="{{ route('desain.index') }}" class="text-white" style="font-size: 20px; text-decoration: none;">
+                                <i class="bi bi-list-ul" style="font-size: 100px"></i><br>Daftar Desain
+                            </a>
+                        </button>
+                    </div>
+
+                    <!-- Button 2 -->
+                    <div class="col-md-4 col-12 d-flex justify-content-center">
+                        <button class="btn btn-primary" style="width: 100%;">
+                            <a href="#" class="text-white" style="font-size: 20px; text-decoration: none;">
                                 <i class="bi bi-clipboard-plus-fill" style="font-size: 100px"></i><br>Perangkingan Baru
                             </a>
                         </button>
                     </div>
-                    <div class="col-md-5 col-12 d-flex justify-content-center">
-                        <button class="btn btn-primary col-md-5">
-                            <a href="/dataset" class="text-white" style="font-size: 20px">
+
+                    <!-- Button 3 -->
+                    <div class="col-md-4 col-12 d-flex justify-content-center">
+                        <button class="btn btn-primary" style="width: 100%;">
+                            <a href="/dataset" class="text-white" style="font-size: 20px; text-decoration: none;">
                                 <i class="bi bi-clipboard-data-fill" style="font-size: 100px"></i><br>Riwayat Perankingan
                             </a>
                         </button>
                     </div>
                 </div>
             </div>
+
 
         </div>
     </section>
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Add Sidebar Menu Items Here -->
 
     <x-maz-sidebar-item name="Dashboard" :link="route('dashboard')" icon="bi bi-grid-fill"></x-maz-sidebar-item>
-    <x-maz-sidebar-item name="Desain" :link="route('desain.index')" icon="bi bi-database-fill"></x-maz-sidebar-item>
+    <x-maz-sidebar-item name="Desain" :link="route('desain.index')" icon="bi bi-list-ul"></x-maz-sidebar-item>
     <x-maz-sidebar-item name="Perangkingan" icon="bi bi-clipboard-fill">
         <x-maz-sidebar-sub-item name="Perangkingan Baru" :link="route('components.alert')"></x-maz-sidebar-sub-item>
         <x-maz-sidebar-sub-item name="Riwayat Perangkingan" :link="route('components.accordion')"></x-maz-sidebar-sub-item>
